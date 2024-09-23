80c61a66f4b6c2e87332a6d86e71f899ae42fe16
vegetablepage <dany.mpdnosrut@gmail.com>
2024-09-23 23:02:22 +0500

    first push

---
+++ backend_api/core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+def home(request):
+    return render(request, 'home.html')
+
+@api_view(['GET', 'POST'])
+def todos_list(request):
+
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        name = request.data.get('name')
+        todo = Todo.objects.create(name=name)
+        serializer = TodoSerializer(todo, many=False)
+        return Response(serializer.data)
+
+
