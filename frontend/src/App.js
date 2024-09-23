9c46dd28a12ac250539cb5899c7f2789d7f41c37
vegetablepage <dany.mpdnosrut@gmail.com>
2024-09-23 23:24:52 +0500

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,60 @@
+import {PlusIcon, TrashIcon} from '@heroicons/react/24/solid'
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function App() {
+    const [todos, setTodos] = useState([])
+    const addTodoHandler = () =>{
+        console.log("click");
+    };
+
+    const deleteTodoHandler = () =>{
+        console.log("Delete");
+    };
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            const {data} = await axios.get("http://127.0.0.1:8000/todos/");
+            setTodos(data);
+        };
+        fetchTodos();
+    }, []);
+
+  return (
+    <div className="bg-slate-900 text-white h-screen relative">
+        <div className="flex flex-col w-full p-10">
+            <h1 className="text-5xl text-center pb-5">Todo App</h1>
+            <div className="flex items-center justify-between bg-slate-700 rounded-xl px-4">
+                <input type="text" className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"/>
+                <i onClick={addTodoHandler}>
+                    <PlusIcon className="h-5 w-5 cursor-pointer hover:opacity-70"/>
+                </i>
+            </div>
+
+            <div className="mt-5 flex flex-col space-y-5 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-10 lg:grid-cols-3">
+                {todos?.map((todo, index) => (
+                    <div key={todo.id}
+                         className="max-w-md mx-auto w-full p-5 h-full rounded-xl bg-blue-500 flex items-center justify-between"
+                    >
+                    <p className="cursor-pointer">
+                        {todo.name}
+                        {" "}
+                        {todo.status && (
+                            <span className="text-xs text-gray-300">(Completed)</span>
+                        )}
+                    </p>
+
+                        <i onClick={() => deleteTodoHandler(todo.id)}>
+                            <TrashIcon className="h-5 w-5 cursor-pointer" fill="white"/>
+                        </i>
+
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
